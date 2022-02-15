c864dc6652792d6a50081124a94eb79b27c4bc97
jarrelp <jarrel.pothoff@gmail.com>
2022-02-15 12:40:53 +0100

    2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-//import clsx from "clsx";
+import clsx from "clsx";
 import { makeStyles } from "@mui/styles";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
@@ -8,84 +8,83 @@ import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import "./styles.css";
 
-// const drawerWidth = 240;
+const drawerWidth = 240;
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: "flex"
-//   },
+const darkMode = "";
 
-//   content: {
-//     flexGrow: 1,
-//     padding: theme.spacing(3),
-//     transition: theme.transitions.create("margin", {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.leavingScreen
-//     }),
-//     marginLeft: -drawerWidth
-//   },
-//   contentShift: {
-//     transition: theme.transitions.create("margin", {
-//       easing: theme.transitions.easing.easeOut,
-//       duration: theme.transitions.duration.enteringScreen
-//     }),
-//     marginLeft: 0
-//   },
-//   drawerHeader: {
-//     display: "flex",
-//     alignItems: "center",
-//     padding: theme.spacing(0, 1),
-//     // necessary for content to be below app bar
-//     ...theme.mixins.toolbar,
-//     justifyContent: "flex-end"
-//   }
-// }));
+const theme = createTheme({
+  palette: {
+    type: darkMode ? "dark" : "light"
+  }
+});
 
-export default function App() {
-  //const classes = useStyles();
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex"
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: -drawerWidth
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    marginLeft: 0
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end"
+  }
+}));
+
+function AppStructure() {
+  const classes = useStyles();
   const [open, setOpen] = useState(true);
   const [darkMode, setDarkMode] = useState(false);
 
-  const handleDrawerToggle = () => {
-    setOpen(!open);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
-  const theme = createTheme({
-    palette: {
-      type: darkMode ? "dark" : "light"
-    }
-  });
-
-  const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
-  };
+  const handleDrawerToggle = () => { setOpen(!open); };
+  const handleDrawerClose = () => { setOpen(false); };
+  const toggleDarkMode = () => { setDarkMode(!darkMode); };
+  
   return (
-    <ThemeProvider theme={theme}>
-      <div 
-      //className={classes.root}
-      >
+      <div className={classes.root}>
         <CssBaseline />
         <Header
           handleDrawerToggle={handleDrawerToggle}
           toggleDarkMode={toggleDarkMode}
           darkMode={darkMode}
         />
-        <Sidebar handleDrawerClose={handleDrawerClose} open={open} />
+        <Sidebar 
+        handleDrawerClose={handleDrawerClose} 
+        open={open} 
+        />
         <main
-          //</div>className={clsx(classes.content, {
-          //  [classes.contentShift]: open
-          //})}
-        >
-          <div 
-          //className={classes.drawerHeader} 
-          />
+           className={clsx(classes.content, {
+             [classes.contentShift]: open
+           })}
+         >
+          <div className={classes.drawerHeader} />
           <Content />
         </main>
       </div>
+  );
+}
+
+export default function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <AppStructure />
     </ThemeProvider>
   );
 }
