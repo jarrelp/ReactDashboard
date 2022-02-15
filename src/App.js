0011103c79b4f81ce0efbc5c521cae1ac9ab5cc9
jarrelp <jarrel.pothoff@gmail.com>
2022-02-15 17:31:20 +0100

    push unfinished work

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ import Content from "./components/Content";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import "./styles.css";
+// import Theme from "./customtheme";
+
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
 
 const drawerWidth = 240;
 
-const darkMode = "light";
+const darkMode = 'light';
 
 const theme = createTheme({
   palette: {
-    type: darkMode ? "dark" : "light"
+    mode: darkMode ? 'dark' : 'light'
   }
 });
 
@@ -48,7 +51,24 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+// export default function ToggleColorMode() {
+//   const [mode, setMode] = React.useState('light');
+//   const colorMode = React.useMemo(
+//     () => ({
+//       toggleColorMode: () => {
+//         setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+//       },
+//     }),
+//     [],
+//   );
+
+// if mode: dark
+// then set darkMode variable to dark
+// else
+// then set darkMode variable to light
+
 function AppStructure() {
+  const colorMode = React.useContext(ColorModeContext);
   const classes = useStyles();
   const [open, setOpen] = useState(true);
   const [darkMode, setDarkMode] = useState(false);
@@ -56,6 +76,8 @@ function AppStructure() {
   const handleDrawerToggle = () => { setOpen(!open); };
   const handleDrawerClose = () => { setOpen(false); };
   const toggleDarkMode = () => { setDarkMode(!darkMode); };
+
+  //schrijf hier verder ifstatement darkmode
   
   return (
       <div className={classes.root}>
@@ -88,113 +110,3 @@ export default function App() {
     </ThemeProvider>
   );
 }
-
-
-
-
-
-
-
-
-// import React, { useState } from "react";
-// import clsx from "clsx";
-// import { makeStyles } from '@mui/styles';
-// import { CssBaseline } from "@mui/material"; //createMuiTheme, 
-// import { createTheme, ThemeProvider } from '@mui/material/styles'; //, adaptV4Theme
-// import Content from "./components/Content";
-// import Header from "./components/Header";
-// import Sidebar from "./components/Sidebar";
-
-// const drawerWidth = 240;
-
-// // const useStyles = makeStyles((theme) => ({
-// //   root: {
-// //     display: "flex"
-// //   },
-
-// //   content: {
-// //     flexGrow: 1,
-// //     padding: theme.spacing(3),
-// //     transition: theme.transitions.create("margin", {
-// //       easing: theme.transitions.easing.sharp,
-// //       duration: theme.transitions.duration.leavingScreen
-// //     }),
-// //     marginLeft: -drawerWidth
-// //   },
-// //   contentShift: {
-// //     transition: theme.transitions.create("margin", {
-// //       easing: theme.transitions.easing.easeOut,
-// //       duration: theme.transitions.duration.enteringScreen
-// //     }),
-// //     marginLeft: 0
-// //   },
-// //   drawerHeader: {
-// //     display: "flex",
-// //     alignItems: "center",
-// //     padding: theme.spacing(0, 1),
-// //     // necessary for content to be below app bar
-// //     ...theme.mixins.toolbar,
-// //     justifyContent: "flex-end"
-// //   }
-// // }));
-
-// const useStyles = makeStyles();
-
-// const theme = createTheme();
-
-// function AppContent(props) {
-//   const classes = useStyles(); // ✅ This is safe because it is called inside ThemeProvider
-//   //return <Card className={classes.root}>...</Card>;
-
-//   const [open, setOpen] = useState(true);
-//   // const [darkMode, setDarkMode] = useState(true);
-
-//   const handleDrawerToggle = () => {
-//     setOpen(!open);
-//   };
-
-//   const handleDrawerClose = () => {
-//     setOpen(false);
-//   };
-
-//   // const toggleDarkMode = () => {
-//   //   setDarkMode(!darkMode);
-//   // };
-
-//   return (
-//     <div className={classes.root}>
-//         <CssBaseline />
-//         <Header
-//           handleDrawerToggle={handleDrawerToggle}
-//           // toggleDarkMode={toggleDarkMode}
-//           // darkMode={darkMode}
-//         />
-//         <Sidebar handleDrawerClose={handleDrawerClose} open={open} />
-//         <main
-//           className={clsx(classes.content, {
-//             [classes.contentShift]: open
-//           })}
-//         >
-//           <div className={classes.drawerHeader} />
-//           <Content />
-//         </main>
-//       </div>
-//   );
-// }
-
-// function App(props) {
-
-//   // const theme = createTheme(({ //adaptV4Theme
-//   //   palette: {
-//   //     type: darkMode ? "dark" : "light"
-//   //   }
-//   //   }));
-
-//   return (
-//     <ThemeProvider theme={theme}>
-//       <AppContent {...props} />
-//     </ThemeProvider>
-//   );
-// }
-
-// export default App;
