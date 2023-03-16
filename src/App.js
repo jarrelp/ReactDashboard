56520e1bb5127881dda2dfdf2ae859bf1480c5ef
jarrelp <jarrel.pothoff@gmail.com>
2023-03-16 15:05:14 +0100

    blabla

--- src/App.js
+++ src/App.js
@@ -1,104 +1,27 @@
-import React, { useState } from "react";
-import clsx from "clsx";
-import { makeStyles } from "@mui/styles";
+import React from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import { CssBaseline } from "@mui/material";
 import Content from "./components/Content";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
 import "./styles.css";
-// import Theme from "./customtheme";
-
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
-
-const drawerWidth = 240;
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex"
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen
-    }),
-    marginLeft: -drawerWidth
-  },
-  contentShift: {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen
-    }),
-    marginLeft: 0
-  },
-  drawerHeader: {
-    display: "flex",
-    alignItems: "center",
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-    justifyContent: "flex-end"
-  }
-}));
 
 function AppStructure() {
-  const colorMode = React.useContext(ColorModeContext);
-  const classes = useStyles();
-  const [open, setOpen] = useState(true);
-  const [darkMode, setDarkMode] = useState(true);
-
-  const handleDrawerToggle = () => { setOpen(!open); };
-  const handleDrawerClose = () => { setOpen(false); };
-  const toggleDarkMode = () => { setDarkMode(!darkMode); };
-  
   return (
-      <div className={classes.root}>
-        <CssBaseline />
-        <Header
-          handleDrawerToggle={handleDrawerToggle}
-          toggleDarkMode={() => { colorMode.toggleColorMode(); toggleDarkMode(); }}
-          darkMode={darkMode}
-        />
-        <Sidebar 
-        handleDrawerClose={handleDrawerClose} 
-        open={open} 
-        />
-        <main
-           className={clsx(classes.content, {
-             [classes.contentShift]: open
-           })}
-         >
-          <div className={classes.drawerHeader} />
-          <Content />
-        </main>
-      </div>
+    <>
+      <Content />
+    </>
   );
 }
 
 export default function App() {
-  const [mode, setMode] = React.useState('light');
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
-      },
-    }),
-    [],
-  );
-
   const theme = React.useMemo(
     () =>
       createTheme({
         palette: {
-          mode,
           primary: {
             main: "#5cbc63",
             contrastText: "#fff",
           },
           secondary: {
-            main: "#000000"
+            main: "#000000",
           },
         },
         typography: {
@@ -113,7 +36,7 @@ export default function App() {
           h5: {
             color: "#484848",
           },
-          fontFamily:"CeraPro-Regular",
+          fontFamily: "CeraPro-Regular",
         },
         components: {
           MuiPaper: {
@@ -150,13 +73,11 @@ export default function App() {
           },
         },
       }),
-    [mode],
+    []
   );
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <AppStructure />
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <ThemeProvider theme={theme}>
+      <AppStructure />
+    </ThemeProvider>
   );
 }
